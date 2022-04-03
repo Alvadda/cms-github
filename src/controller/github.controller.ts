d9216b925aad56195b6f783d09fb1ba12a7d6b7c
Alvadda <58438582+Alvadda@users.noreply.github.com>
2022-04-03 18:44:07 +0200

    save content in cache

--- src/controller/github.controller.ts
+++ src/controller/github.controller.ts
@@ -1,21 +1,43 @@
 import { Request, Response } from 'express'
 import axios from 'axios'
+import NodeCache from 'node-cache'
+const cache = new NodeCache({ stdTTL: 600, checkperiod: 300 })
 
 const USER = 'Alvadda'
 const REPO = 'cms-github'
 const DIR = 'content'
 
+const CK_DIR = 'CK_CONTENT'
+const CK_FILE = 'CK_FILE_'
+
+const generateCKFile = (fileName: string) => CK_FILE + fileName
+
 export const getContent = async (req: Request, res: Response) => {
+  if (cache.has(CK_DIR)) {
+    const dir = cache.get(CK_DIR)
+    console.log('Folder from Cache')
+    return res.json(dir)
+  }
+
   const result = await axios.get(`https://api.github.com/repos/${USER}/${REPO}/contents/${DIR}`)
 
-  console.log(result.data)
+  cache.set(CK_DIR, result.data)
+  console.log('Folder from Github')
   res.json(result.data)
 }
 
 export const getFile = async (req: Request, res: Response) => {
   const { file } = req.params
+  const cacheKey = generateCKFile(file)
+  if (cache.has(cacheKey)) {
+    const file = cache.get(cacheKey)
+    console.log('File from Cache')
+    return res.json(file)
+  }
+
   const result = await axios.get(`https://raw.githubusercontent.com/${USER}/${REPO}/main/${DIR}/${file}.json`)
 
-  console.log(result.data)
+  cache.set(cacheKey, result.data)
+  console.log('File from Github')
   res.json(result.data)
 }
